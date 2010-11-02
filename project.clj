62bda8d22962049a7824dcc5be03d163cb0346ec
Roman Scherer <roman@burningswell.com>
2010-11-02 19:28:59 +0100

    Configured logging.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Rails-like Database Migration for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.xerial/sqlite-jdbc "3.6.20.1"]
+  :dev-dependencies [[log4j "1.2.15" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
+                     [org.xerial/sqlite-jdbc "3.6.20.1"]
                      [swank-clojure "1.2.1"]])
