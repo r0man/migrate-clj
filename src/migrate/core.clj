7be138ac3abdef67d7bd686091cc0e367c722f1f
Roman Scherer <roman@burningswell.com>
2010-11-02 21:20:46 +0100

    Added metadata to database.

--- src/migrate/core.clj
+++ src/migrate/core.clj
@@ -1,16 +1,14 @@
 (ns migrate.core
-  (:import java.io.File [java.sql DriverManager SQLException])
-  (:refer-clojure :exclude (replace))
+  (:import java.sql.SQLException)
   (:require [clojure.contrib.sql :as sql])
-  (:use [clojure.string :only (replace)]
-        [clojure.contrib.def :only (defvar)]
+  (:use [clojure.contrib.def :only (defvar)]
         [clojure.contrib.logging :only (info)]))
 
-(defvar *migrations*
-  (atom {}) "The defined migrations by name.")
+(defvar *migrations* (atom {})
+  "All migrations by name.")
 
-(defvar *version-table*
-  "schema_versions" "The table name for the schema versions.")
+(defvar migration-table "schema_migrations"
+  "The table name for the migration metadata.")
 
 (defn str= [s1 s2] (= (.compareTo s1 s2) 0))
 (defn str< [s1 s2] (< (.compareTo s1 s2) 0))
@@ -18,41 +16,92 @@
 (defn str<= [s1 s2] (<= (.compareTo s1 s2) 0))
 (defn str>= [s1 s2] (>= (.compareTo s1 s2) 0))
 
-(defn create-version-table
-  "Creates the schema version table."
-  [] (sql/create-table *version-table* [:id :string "PRIMARY KEY"]))
+(defn create-migration-table
+  "Create the database table that holds the migration metadata."
+  [] (sql/create-table
+      migration-table
+      [:version :string "PRIMARY KEY NOT NULL"]
+      [:description :string]
+      [:created_at :time]))
 
-(defmacro defmigration [name doc-string up-fn down-fn]
-  `(let [migration# {:name ~name :doc ~doc-string :up #(~@up-fn) :down #(~@down-fn)}]
-     (swap! *migrations* assoc (:name migration#) migration#)))
+(defn drop-migration-table
+  "Drop the database table that holds the migration metadata."
+  [] (sql/drop-table migration-table))
 
-(defn drop-version-table
-  "Drop the schema version table."
-  [] (sql/drop-table *version-table*))
+(defn insert-migration
+  "Insert the migration's metadata into the database."
+  [migration]  
+  (sql/insert-records migration-table (-> migration (dissoc :up :down))))
 
-(defn find-applicable [from-version to-version]
-  (if (str<= from-version to-version)
-    (filter #(and (str< from-version (:name %)) (str<= (:name %) to-version))
-            (sort-by :name (vals @*migrations*)))
-    (reverse (find-applicable to-version from-version))))
+(defn delete-migration
+  "Delete the migration's metadata from the database."
+  [migration] (sql/delete-rows migration-table ["version=?" (:version migration)]))
+
+(defn latest-migration
+  "Returns the latest (the most recent) migration."
+  [] (last (sort-by :version (vals @*migrations*))))
+
+(defn latest-version
+  "Returns the version of the latest (the most recent) migration."
+  [] (:version (latest-migration)))
+
+(defmacro defmigration [name description up-form down-form]
+  `(let [migration# {:version ~name :description ~description :up #(~@up-form) :down #(~@down-form)}]
+     (swap! *migrations* assoc (:version migration#) migration#)))
+
+(defn find-applicable-migrations
+  "Returns all migrations that have to be run to migrate from
+  from-version to to-version."
+  [from-version to-version]
+  (let [from-version (or from-version "")
+        to-version (:version (latest-migration))]
+    (if (str<= from-version to-version)
+      (filter #(and (str< from-version (:version %)) (str<= (:version %) to-version))
+              (sort-by :version (vals @*migrations*)))
+      (reverse (find-applicable-migrations to-version from-version)))))
+
+(defn find-migration-by-version
+  "Returns the migration with the given version."
+  [version] (first (filter #(= (:version %) version) (vals @*migrations*))))
 
 (defn select-current-version
-  "Returns the current schema version."
+  "Returns the current schema version, or nil if no migration has been
+  run yet."
   [] (sql/with-query-results result-set
-       [(str "SELECT MAX(id) AS version FROM " *version-table*)]
+       [(str "SELECT MAX(version) AS version FROM " migration-table)]
        (:version (first result-set))))
 
-(defn version-table? []
-  "Returns true if the *version-table* exists, otherwise false."
+(defn migration-table? []
+  "Returns true if the migration-table exists, otherwise false."
   (try (do (select-current-version) true)
        (catch SQLException _ false)))
 
-(defn run []
-  (let [current (or (select-current-version) "")
-        target (last (sort (keys @*migrations*)))
-        direction (if (str<= current target ) :up :down)]
-    (doseq [migration (find-applicable current target)]
-      (info (str "Running migration: " (:name migration)))
-      (info (str "      Description: " (:doc migration)))
-      ((direction migration)))))
+(defn- run-up
+  "Run the migration by invoking the fn stored under the :up key and
+  insert the metadata into the migration table."
+  [migration]
+  ((:up migration))
+  (insert-migration migration))
 
+(defn- run-down 
+  "Run the migration by invoking the fn stored under the :down key and
+  delete the metadata into the migration table."
+  [migration]
+  ((:down migration))
+  (delete-migration migration))
+
+(defn run
+  "Run the migrations from the current to the target version. If
+  target version is nil the migartions run up to the latest version."
+  [& [target-version]]
+  (if-not (migration-table?)
+    (create-migration-table))
+  (let [current-version (or (select-current-version) "")
+        target-version (or target-version (latest-version))]
+    (sql/transaction
+     (doseq [migration (find-applicable-migrations current-version target-version)]
+       (info (str "Running migration: " (:version migration)))
+       (info (str "      Description: " (:description migration)))
+       (if (str<= current-version target-version )
+         (run-up migration)
+         (run-down migration))))))
--- test/migrate/test/core.clj
+++ test/migrate/test/core.clj
@@ -18,66 +18,89 @@
      (sql/with-connection *database* ~@body)))
 
 (defmacro with-version-table [& body]
-  `(try (do (create-version-table) ~@body) 
-        (finally (drop-version-table))))
+  `(try (do (create-migration-table) ~@body) 
+        (finally (drop-migration-table))))
 
 (defmigration "2010-11-01 21:30:10"
-  "Create the continents table."      
+  "Create continent table."      
   (sql/create-table
    "continents"
    [:id :string "PRIMARY KEY"])    
   (sql/drop-table :continents))
 
 (defmigration "2010-11-01 21:32:45"
-  "Create the countries table."    
+  "Create country table."    
   (sql/create-table
    "countries"
    [:id :string "PRIMARY KEY"]
    [:continent_id :string])    
   (sql/drop-table :continents))
 
-(dbtest test-create-version-table
-  (is (create-version-table))
-  (drop-version-table))
+(deftest test-latest-migration
+  (is (= (latest-migration) (find-migration-by-version "2010-11-01 21:32:45"))))
 
-(dbtest test-drop-version-table
-  (create-version-table)
-  (is (drop-version-table)))
+(deftest test-latest-version
+  (is (= (latest-version) "2010-11-01 21:32:45")))
+
+(deftest test-find-migration-by-version
+  (is (nil? (find-migration-by-version "unknown version")))
+  (are [version]
+    (is (= (:version (find-migration-by-version version)) version))
+    "2010-11-01 21:30:10"
+    "2010-11-01 21:32:45"))
+
+(dbtest test-create-migration-table
+  (is (create-migration-table))
+  (drop-migration-table))
+
+(dbtest test-drop-migration-table
+  (create-migration-table)
+  (is (drop-migration-table)))
+
+(dbtest test-migration-table?
+  (is (not (migration-table?)))
+  (create-migration-table)
+  (is (migration-table?))
+  (drop-migration-table)
+  (is (not (migration-table?))))
+
+(dbtest test-insert-migration
+  (with-version-table
+    (doseq [version ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]]
+      (let [migration (find-migration-by-version version)]
+        (insert-migration migration)
+        (is (= (select-current-version) (:version migration)))))))
+
+(dbtest test-delete-migration
+  (with-version-table
+    (let [migrations (map find-migration-by-version ["2010-11-01 21:30:10" "2010-11-01 21:32:45"])]
+      (doall (map insert-migration migrations))
+      (doseq [migration (reverse migrations)]
+        (is (= (select-current-version) (:version migration)))
+        (delete-migration migration)
+        (is (not (= (select-current-version) (:version migration)))))
+      (is (nil? (select-current-version))))))
+
+(dbtest test-find-applicable-migrations
+  (with-version-table
+    (are [from to expected]
+      (is (= (find-applicable-migrations from to)
+             (map find-migration-by-version expected)))
+      nil nil ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]
+      ""  ""  ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]      
+      nil "2010-11-01 21:32:45" ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]
+      "" "2010-11-01 21:32:45" ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]
+      "2010-11-01 21:30:10" "2010-11-01 21:32:45" ["2010-11-01 21:32:45"])))
+
+(dbtest test-run  
+  (run)
+  (is (= (select-current-version) (:version (latest-migration)))))
 
 (dbtest test-select-current-version
   (is (thrown? SQLException (select-current-version)))
   (with-version-table
     (is (nil? (select-current-version)))
-    (sql/insert-records *version-table* {:id "201011012120"})
+    (sql/insert-records migration-table {:version "201011012120"})
     (is (= (select-current-version) 201011012120))
-    (sql/insert-records *version-table* {:id "201012012120"})
+    (sql/insert-records migration-table {:version "201012012120"})
     (is (= (select-current-version) 201012012120))))
-
-(dbtest test-version-table?
-  (create-version-table)
-  (is (version-table?))
-  (drop-version-table)
-  (is (not (version-table?))))
-
-;; (dbtest test-find-applicable
-;;   (is (thrown? Exception (find-applicable :invalid-direction)))
-;;   (with-version-table
-;;     (is (= (find-applicable "2010-11-01 21:30:10" "2010-11-01 21:30:10")
-;;            []))
-;;     (is (= (find-applicable "2010-11-01 21:30:10" "2010-11-01 21:32:45")
-;;            ["2010-11-01 21:32:45" ]))
-;;     (is (= (find-applicable "2010-11-01 21:32:45" "2010-11-01 21:30:10")
-;;            ["2010-11-01 21:32:45" ]))
-;;     (is (= (find-applicable "2010-11-01 21:30:09" "2010-11-01 21:32:45")
-;;            ["2010-11-01 21:30:10" "2010-11-01 21:32:45"]))
-;;     (is (= (find-applicable "2010-11-01 21:32:45" "2010-11-01 21:30:09")
-;;            ["2010-11-01 21:32:45" "2010-11-01 21:30:10"]))))
-
-(dbtest tet-run
-  (with-version-table
-    (run)))
-
-;; (deftest replace-me
-;;   (sql/with-connection *database*
-;;     (sql/with-query-results result-set
-;;       "SELECT * FROM test")))
