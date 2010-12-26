c465a7b2789d009179754e8e951db057bea235bb
roman <roman@soundclaat.localdomain>
2010-12-26 17:11:38 +0100

    Upgraded autodoc.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :url "http://github.com/r0man/migrate-clj"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[autodoc "0.7.1-SNAPSHOT"]
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
                      [log4j "1.2.15" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                      [org.xerial/sqlite-jdbc "3.6.20.1"]
                      [postgresql/postgresql "8.4-701.jdbc4"]
