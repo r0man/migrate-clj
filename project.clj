aaed1a207ccd0f9affa8687c9ae09ef773120184
Roman <roman@burningswell.com>
2011-12-27 10:53:26 +0100

    Switched to java.jdbc 0.0.7.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :copyright "Copyright (c) 2010 Roman Scherer"}
   :url "http://github.com/r0man/migrate-clj"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.0.5"]
+                 [org.clojure/java.jdbc "0.0.7"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[org.slf4j/slf4j-log4j12 "1.5.10"]
                      [org.xerial/sqlite-jdbc "3.7.2"]
