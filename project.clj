7b30be91bb2af740cef131d3d196008798919e5f
Roman <roman@burningswell.com>
2012-03-13 20:32:31 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,20 +1,22 @@
-(defproject migrate "0.0.6-SNAPSHOT"
+(defproject migrate/migrate "0.0.7-SNAPSHOT"
   :description "Rails-like Database Migrations for Clojure"
-  :autodoc {:name "Rails-like Database Migrations for Clojure"
-            :author "Roman Scherer"
-            :web-src-dir "http://github.com/r0man/migrate-clj/blob/"
-            :web-home "http://github.com/r0man/migrate-clj"
-            :copyright "Copyright (c) 2010 Roman Scherer"}
   :url "http://github.com/r0man/migrate-clj"
-  :dependencies [[lein-env "0.0.2-SNAPSHOT"]
+  :autodoc {:name "Rails-like Database Migrations for Clojure",
+            :author "Roman Scherer",
+            :web-src-dir "http://github.com/r0man/migrate-clj/blob/",
+            :web-home "http://github.com/r0man/migrate-clj",
+            :copyright "Copyright (c) 2010 Roman Scherer"}
+  :dependencies [[lein-env "0.0.3-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.0.7"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
-  :dev-dependencies [[lein-env "0.0.2-SNAPSHOT"]
-                     [leiningen/leiningen "1.6.2"]
-                     [org.clojure/java.jdbc "0.0.7"]
-                     [org.slf4j/slf4j-log4j12 "1.5.6"]
-                     [postgresql/postgresql "9.1-901.jdbc4"]]
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :dependencies [[leiningen/leiningen "2.0.0-preview2"]
+                                  [org.clojure/java.jdbc "0.0.7"]
+                                  [org.slf4j/slf4j-log4j12 "1.5.6"]
+                                  [postgresql/postgresql "9.1-901.jdbc4"]]}}
+  :plugins [[lein-env "0.0.3-SNAPSHOT"]]
+  :min-lein-version "2.0.0"
   :migrate [migrate.examples])
