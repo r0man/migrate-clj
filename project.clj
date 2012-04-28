904a746f5f07afe1c0fc69909dcc5ea978225866
Roman <roman@burningswell.com>
2012-04-28 16:11:58 +0200

    Switched to slf4j-log4j12 1.6.4.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject migrate/migrate "0.0.7-SNAPSHOT"
   :description "Rails-like Database Migrations for Clojure"
+  :min-lein-version "2.0.0"
   :url "http://github.com/r0man/migrate-clj"
   :autodoc {:name "Rails-like Database Migrations for Clojure",
             :author "Roman Scherer",
@@ -10,14 +11,12 @@
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.4"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.slf4j/slf4j-log4j12 "1.5.6"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[leiningen/leiningen "2.0.0-preview2"]
                                   [org.clojure/java.jdbc "0.1.4"]
-                                  [org.slf4j/slf4j-log4j12 "1.5.6"]
+                                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                                   [postgresql/postgresql "9.1-901.jdbc4"]]}}
   :plugins [[lein-env "0.0.3-SNAPSHOT"]]
   :hooks [leiningen.hooks.env]
-  :min-lein-version "2.0.0"
   :migrate [migrate.examples])
