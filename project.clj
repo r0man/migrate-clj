a4e6766e769439618e2420ffc0980d227a78e970
Roman <roman@burningswell.com>
2012-05-03 22:54:44 +0200

    Switched to java.jdbc 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migrate/migrate "0.0.7-SNAPSHOT"
+(defproject migrate/migrate "0.0.8-SNAPSHOT"
   :description "Rails-like Database Migrations for Clojure"
   :min-lein-version "2.0.0"
   :url "http://github.com/r0man/migrate-clj"
@@ -9,14 +9,14 @@
             :copyright "Copyright (c) 2010 Roman Scherer"}
   :dependencies [[lein-env "0.0.3-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/java.jdbc "0.1.4"]
+                 [org.clojure/java.jdbc "0.2.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies [[leiningen/leiningen "2.0.0-preview2"]
-                                  [org.clojure/java.jdbc "0.1.4"]
+                   :dependencies [[org.clojure/java.jdbc "0.2.0"]
                                   [org.slf4j/slf4j-log4j12 "1.6.4"]
                                   [postgresql/postgresql "9.1-901.jdbc4"]]}}
+  :eval-in :leiningen
   :plugins [[lein-env "0.0.3-SNAPSHOT"]]
   :hooks [leiningen.hooks.env]
   :migrate [migrate.examples])
