37ec1945975451e2e452723881acc605a76f7068
Roman <roman@burningswell.com>
2012-05-12 21:53:51 +0200

    Fix version of migrate.lein submodule.

--- migrate.lein/project.clj
+++ migrate.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject migrate.lein "0.1.0-SNAPSHOT"
+(defproject migrate/migrate.lein "0.0.9-SNAPSHOT"
   :description "Leiningen plugin for Migrate"
   :url "http://github.com/r0man/migrate-clj"
   :license {:name "Eclipse Public License"
