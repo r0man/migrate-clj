fe5b10504afc5598f09e96843e2f7c06b813c1f8
Roman Scherer <roman.scherer@burningswell.com>
2012-08-30 22:27:40 +0200

    Switched to environ 0.3.0.

--- migrate.core/project.clj
+++ migrate.core/project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[environ "0.2.1"]
+  :dependencies [[environ "0.3.0"]
                  [clj-time "0.4.3"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
@@ -14,7 +14,5 @@
                                   [org.slf4j/slf4j-log4j12 "1.6.4"]
                                   [postgresql/postgresql "9.1-901.jdbc4"]]}}
   :eval-in :leiningen
-  ;; :plugins [[environ/environ.lein "0.2.1"]]
-  ;; :hooks [environ.leiningen.hooks]
-  ;; :migrate [migrate.test.examples]
-  )
+  :plugins [[environ/environ.lein "0.3.0"]]
+  :hooks [environ.leiningen.hooks])
