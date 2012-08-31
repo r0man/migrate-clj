b6dd996bb8aacfc75ed761d83c5acce9ae6574b7
Roman Scherer <roman.scherer@burningswell.com>
2012-08-31 13:59:51 +0200

    Added resolve-db-spec.

--- migrate.core/src/migrate/connection.clj
+++ migrate.core/src/migrate/connection.clj
@@ -6,17 +6,16 @@
   "Returns the string to quote identifiers from the `connection` meta data."
   [connection] (.getIdentifierQuoteString (.getMetaData (jdbc/connection))))
 
+(defn resolve-db-spec [db-spec]
+  (cond
+   (keyword? db-spec)
+   (env db-spec)
+   (and (string? db-spec)
+        (= \: (first db-spec)))
+   (env (keyword (apply str (rest db-spec))))
+   :else db-spec))
+
 (defmacro with-connection [db-spec & body]
-  `(jdbc/with-connection ~db-spec
-     (jdbc/with-quoted-identifiers (identifier-quote-string (jdbc/connection)))
-     ~@body))
-
-;; (with-connection :bs-database
-;;   (identifier-quote-string (jdbc/connection)))
-
-;; (identifier-quote-string (jdbc/connection))
-
-;; (java.net.URI. "postgresql://burningswell:burningswell13+@localhost/burningswell_development")
-;; (java.net.URI. "postgresql")
-
-;; (prn (env :bs-database))
+  `(jdbc/with-connection (resolve-db-spec ~db-spec)
+     (jdbc/with-quoted-identifiers (identifier-quote-string (jdbc/connection))
+       ~@body)))
--- migrate.core/test/migrate/test/connection.clj
+++ migrate.core/test/migrate/test/connection.clj
@@ -3,6 +3,23 @@
             [clojure.test :refer :all]
             [migrate.connection :refer :all]))
 
+(def db-spec "postgresql://localhost/migrate_test")
+
 (deftest test-identifier-quote-string
-  (with-connection "postgresql://localhost/migrate_test"
+  (with-connection db-spec
     (identifier-quote-string (jdbc/connection))))
+
+(deftest test-resolve-db-spec
+  (are [db-spec expected]
+    (is (=  expected (resolve-db-spec db-spec)))
+    nil nil
+    "" ""
+    "x" "x"
+    :migrate-db "postgresql://localhost/migrate_development"
+    ":migrate-db" "postgresql://localhost/migrate_development"
+    "postgresql://localhost/migrate_development" "postgresql://localhost/migrate_development"))
+
+(deftest test-with-connection
+  (with-connection db-spec
+    (is (jdbc/connection))
+    (is (= "\"x\"" (@#'jdbc/*as-str* "x")))))
