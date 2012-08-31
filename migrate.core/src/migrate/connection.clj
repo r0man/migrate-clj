fe83f5948b38e4e63e13aae57e16e187e48b6e1b
Roman Scherer <roman.scherer@burningswell.com>
2012-08-31 14:02:21 +0200

    Docs.

--- migrate.core/src/migrate/connection.clj
+++ migrate.core/src/migrate/connection.clj
@@ -6,7 +6,9 @@
   "Returns the string to quote identifiers from the `connection` meta data."
   [connection] (.getIdentifierQuoteString (.getMetaData (jdbc/connection))))
 
-(defn resolve-db-spec [db-spec]
+(defn resolve-db-spec
+  "Reolve `db-spec` via environ or return `db-spec`."
+  [db-spec]
   (cond
    (keyword? db-spec)
    (env db-spec)
@@ -15,7 +17,9 @@
    (env (keyword (apply str (rest db-spec))))
    :else db-spec))
 
-(defmacro with-connection [db-spec & body]
+(defmacro with-connection
+  "Evaluates body in the context of a new connection to a database."
+  [db-spec & body]
   `(jdbc/with-connection (resolve-db-spec ~db-spec)
      (jdbc/with-quoted-identifiers (identifier-quote-string (jdbc/connection))
        ~@body)))
