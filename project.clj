6c7f1cd258f880ccc7466d42a1562afa6c073be5
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:27:00 +0100

    Switched to clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :plugins [[lein-sub "0.2.1"]]
   :sub ["migrate.core" "migrate.lein" "migrate.example"])
